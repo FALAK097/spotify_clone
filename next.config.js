867c9def689c3757af172bc19759fbdd734228ec
falak <falakgala09@gmail.com>
2023-08-18 16:53:22 +0530

    feat: fetched songs and display the list on search

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['emsozvogokciihokfqix.supabase.co'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
